13d37026b8ce21e05635a41adb5ff8d0555a1a68
Lauri Koskela <luryus@users.noreply.github.com>
2021-10-03 11:14:56 +0300

    Sanitize key name characters

--- profiles.c
+++ profiles.c
@@ -15,6 +15,15 @@ void prof_set_current_key_names(const char *key_names) {
     // key_names must be a 12-element array of 4-element char arrays (no null terminators)
     // Just copy it into its place
     memcpy(current_key_names, key_names, 4 * 12);
+
+    // Then ensure that all the characters are valid.
+    // Replace invalid characters with spaces.
+    for (int i = 0; i < 4 * 12; i++) {
+        char c = current_key_names[i];
+        if (c < 32 || c > 126) {
+            current_key_names[i] = ' ';
+        }
+    }
 }
 
 char *prof_get_current_name() {
